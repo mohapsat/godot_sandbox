7e8adb503778d33e9194c7d02ac17798f642cf91
Satya Mohapatra <mohapsat@gmail.com>
2018-07-12 16:05:36 -0700

    G8 Tweens and Timers
    Tweens to add effects to objects and timers for measure durations, create regular repeating effects, and lots more.

--- GODOT101/G5_PlayerControlledSprite/gem.gd
+++ GODOT101/G5_PlayerControlledSprite/gem.gd
@@ -1,12 +1,27 @@
 extends Area2D
 
+onready var sprite = $"Sprite"
+onready var effect = $"effect"
+
 signal gem_grabbed
 
 func _ready():
-	pass
+#	adding tween effect to gem, use interpolate property to
+#	modify scale of sprite
+	effect.interpolate_property(sprite, "scale", 
+		sprite.get_scale(), Vector2(2.0, 2.0), 0.15,
+		Tween.TRANS_QUAD, Tween.EASE_OUT)
+#	change opacity using visibilty / modulate property for sprite
+	effect.interpolate_property(sprite, "modulate", 
+		Color(1,1,1,1), Color(1, 1, 1, 0), 0.15,
+		Tween.TRANS_QUAD, Tween.EASE_OUT)
 
 func _on_gem_area_entered(area):
 #	print(area.get_name())
 	if area.get_name() == 'player':
-		queue_free()
 		emit_signal("gem_grabbed")
+		effect.start()
+		
+
+func _on_effect_tween_completed(object, key):
+	queue_free()
--- GODOT101/G5_PlayerControlledSprite/main.gd
+++ GODOT101/G5_PlayerControlledSprite/main.gd
@@ -3,6 +3,8 @@ extends Node
 # preload gem texture
 onready var gem = preload("res://gem.tscn")
 onready var level_label = preload("res://level_label.tscn")
+onready var timer_label = get_node("timer_label")
+onready var game_timer = get_node("game_timer")
 
 var screensize
 var score = 0
@@ -10,16 +12,20 @@ var level = 1
 
 func _ready():
 	screensize = get_viewport().size
-	spawn_gems(10)
-	
+	spawn_gems(3)
 
 
 func _process(delta):
 	
+	timer_label.text = str(int(game_timer.get_time_left()))
+		
 	if $gem_container.get_child_count() == 0:
 		level += 1
 		$level_label.text = "LEVEL " + str(level)
-		spawn_gems(level * 10)
+		spawn_gems(level * 3)
+		game_timer.start()
+		
+		
 		
 func spawn_gems(num):
 	randomize() # randomize rand_range seed
@@ -35,4 +41,7 @@ func spawn_gems(num):
 func _on_gem_grabbed():
 	score += 1
 	$score_label.text = "SCORE: " + str(score)	
-	print(score)
+
+
+func _on_game_timer_timeout():
+	pass # replace with function body
