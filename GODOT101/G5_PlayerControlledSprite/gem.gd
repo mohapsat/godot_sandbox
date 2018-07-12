6a51a732d758376e823e598a3ed4ae2ed15a56fe
Satya Mohapatra <mohapsat@gmail.com>
2018-07-12 02:58:36 -0700

    G7_Signals
    Used signals to calculate score. Very useful

--- GODOT101/G5_PlayerControlledSprite/gem.gd
+++ GODOT101/G5_PlayerControlledSprite/gem.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+signal gem_grabbed
 
 func _ready():
 	pass
@@ -8,3 +9,4 @@ func _on_gem_area_entered(area):
 #	print(area.get_name())
 	if area.get_name() == 'player':
 		queue_free()
+		emit_signal("gem_grabbed")
--- GODOT101/G5_PlayerControlledSprite/main.gd
+++ GODOT101/G5_PlayerControlledSprite/main.gd
@@ -2,6 +2,8 @@ extends Node
 
 # preload gem texture
 onready var gem = preload("res://gem.tscn")
+onready var level_label = preload("res://level_label.tscn")
+
 var screensize
 var score = 0
 var level = 1
@@ -24,5 +26,13 @@ func spawn_gems(num):
 	for i in range(num):
 		var g = gem.instance()
 		$gem_container.add_child(g)
+		
+		g.connect("gem_grabbed", self, "_on_gem_grabbed")
+		
 		g.position.x = rand_range(0,screensize.x)
 		g.position.y = rand_range(0,screensize.y)
+		
+func _on_gem_grabbed():
+	score += 1
+	$score_label.text = "SCORE: " + str(score)	
+	print(score)
