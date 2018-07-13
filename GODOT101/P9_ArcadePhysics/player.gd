0352b4d6b53d31e1887f20caccd89084c54faf83
Satya Mohapatra <mohapsat@gmail.com>
2018-07-13 01:17:50 -0700

    MovingKinematicsBody
    Gravity, Friction, Accelaration..

--- GODOT101/P9_ArcadePhysics/player.gd
+++ GODOT101/P9_ArcadePhysics/player.gd
@@ -4,8 +4,10 @@ onready var sprite = get_node("sprite")
 
 const UP = Vector2(0, -1)
 const GRAVITY = 20
-const SPEED = 400
+const ACCELARATION = 50
+const MAX_SPEED = 400
 const JUMP_HEIGHT = -500
+const FRICTION = 0.1
 
 var motion = Vector2()
 
@@ -14,22 +16,31 @@ func _physics_process(delta):
 	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		motion.x += ACCELARATION
+		motion.x = min(motion.x, MAX_SPEED)
+					
 		sprite.flip_h = false
 		sprite.play("running")
 		
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		motion.x -= ACCELARATION
+		motion.x = max(motion.x, -MAX_SPEED)
+		
 		sprite.flip_h = true
 		sprite.play("running")
 	else:
-		motion.x = 0
 		sprite.play("idle")
-		
+#		motion.x = 0
+		motion.x = lerp(motion.x, 0, FRICTION) #smooth stop
+		# weight in lerp can be used for friction
+				
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
 	else:
-		sprite.play("jump")
+		if motion.y < 0:
+			sprite.play("jump_up")
+		else:
+			sprite.play("jump_fall")
 			
 	motion = move_and_slide(motion, UP)
